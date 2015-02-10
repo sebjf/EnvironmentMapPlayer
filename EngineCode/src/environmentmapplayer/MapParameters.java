11dab59670bdc5030eb296fbf6527b34d24114ae
Sebastian <demo@localhost.localdomain>
2015-02-10 07:06:10 -0800

    * moved map parameters into dedicated class, although they are still hardcoded * design now uses cache * design now selects mip map level based on sampling

---
+++ EngineCode/src/environmentmapplayer/MapParameters.java
@@ -0,0 +1,13 @@
+package environmentmapplayer;
+
+public final class MapParameters {
+
+	public static int tile_width_in_pixels 	= 8;
+
+	public static int max_mip_level			= 9; //mip level cannot be so high that the map has a higher resolution than the source
+	public static int min_mip_level			= 3; //mip level cannot be so low that the map is smaller than the tile size
+
+
+	public static int pixel_size_in_bytes 	= 4;
+
+}
