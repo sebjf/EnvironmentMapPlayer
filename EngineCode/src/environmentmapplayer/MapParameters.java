12cef3d4a2a67ed830cb6e086505ee2e9b318dd8
Sebastian <demo@localhost.localdomain>
2015-03-16 11:36:53 -0700

    * built with tiled and mipped map ready for hw

--- EngineCode/src/environmentmapplayer/MapParameters.java
+++ EngineCode/src/environmentmapplayer/MapParameters.java
@@ -4,9 +4,10 @@ public final class MapParameters {
 
 	public static int tile_width_in_pixels 	= 8;
 
-	public static int max_mip_level			= 9; //mip level cannot be so high that the map has a higher resolution than the source
+	public static int max_mip_level			= 10; //mip level cannot be so high that the map has a higher resolution than the source
 	public static int min_mip_level			= 3; //mip level cannot be so low that the map is smaller than the tile size
 
+	public static int num_faces				= 8;
 
 	public static int pixel_size_in_bytes 	= 4;
 
