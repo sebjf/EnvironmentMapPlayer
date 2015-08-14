2b6e070ae15fa6902f0471a619988a543ff7cc19
System User <sfriston@nxws38.office.maxeler.com>
2015-08-14 10:31:30 +0100

    * rebuilt with num_faces specified as 6 not 9

--- EngineCode/src/environmentmapplayer/EnvironmentParameters.java
+++ EngineCode/src/environmentmapplayer/EnvironmentParameters.java
@@ -8,7 +8,7 @@ public final class EnvironmentParameters {
 	public static int max_mip_level			= 11; //mip level cannot be so high that the map has a higher resolution than the source
 	public static int min_mip_level			= 3;  //mip level cannot be so low that the map is smaller than the tile size
 
-	public static int num_faces				= 9;
+	public static int num_faces				= 6;
 
 	public static int pixel_size_in_bytes 	= 4;
 
